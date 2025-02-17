700073d0be98f3b224c8049697d7c2e8bc72ec93
EasinTanvir <easintanvir102@gmail.com>
2025-02-17 23:58:51 +0600

    added admin route and layout

---
+++ src/app/(routes)/[locale]/(admin)/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AdminLaout({ children }) {
+  return (
+    <div className="py-28">
+      <h1>AdminLaout</h1>
+
+      {children}
+    </div>
+  );
+}
+export default AdminLaout;
