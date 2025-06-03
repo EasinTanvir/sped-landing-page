8766d62154a945962e13c6eb268a014031da7e5e
EasinTanvir <easintanvir102@gmail.com>
2025-06-03 22:39:42 +0600

    fixed a small bugs

--- src/pageSection/landingPage/masterChefs/MasterChef.jsx
+++ src/pageSection/landingPage/masterChefs/MasterChef.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 import { lobster } from "@/app/(routes)/[locale]/layout";
-import { Container, H2 } from "@/index";
+import { H2 } from "@/index";
 // import ChefWarpper from "./ChefWarpper";
 import SpedBanner from "../newsLetter/spedbanner/SpedBanner";
+import { getTranslations } from "next-intl/server";
 
-const MasterChef = () => {
+const MasterChef = async () => {
+  const t = await getTranslations("spedBanner");
   return (
     <div className="bg-colors-bannerBgColor sm:py-20  py-12">
       <H2
         className={`lg:text-6xl sm:text-5xl text-4xl text-center font-bold italic sm:!leading-[80px] !leading-[50px] ${lobster.className}`}
       >
-        Join Sped Today
+        {t("mainTitle")}
       </H2>
 
       {/* <ChefWarpper /> */}
